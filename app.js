07e2903c5f17c96765a5d0cc1080956277f715b6
Bello Okiki <okiki_bello@yahoo.com>
2023-04-10 14:56:04 +0000

    changes

--- app.js
+++ app.js
@@ -4,19 +4,37 @@ const books = [
       title: "The Great Gatsby",
       author: "F. Scott Fitzgerald",
       price: 12.99,
-      image: "https://images-na.ssl-images-amazon.com/images/I/41pwrbhqNkL._SY291_BO1,204,203,200_QL40_FMwebp_.jpg",
+      image: "https://th.bing.com/th/id/R.7c5697ee72aae7fe607f69d223740452?rik=sgWxstAv8tg2Eg&riu=http%3a%2f%2fd28hgpri8am2if.cloudfront.net%2fbook_images%2fonix%2fcvr9781471173936%2fthe-great-gatsby-9781471173936_hr.jpg&ehk=Jz0jBOB5sZkGZ0GWHtNFPKtFLQNRZEIuO6rAtLbpOOA%3d&risl=&pid=ImgRaw&r=0",
     },
     {
       title: "To Kill a Mockingbird",
       author: "Harper Lee",
       price: 10.99,
-      image: "https://images-na.ssl-images-amazon.com/images/I/51UoqRAxwZL._SX324_BO1,204,203,200_.jpg",
+      image: "https://cdn2.penguin.com.au/covers/original/9780434020485.jpg",
     },
     {
-      title: "1984",
-      author: "George Orwell",
+      title: "Arrow of God",
+      author: "Chinua Achebe",
       price: 8.99,
-      image: "https://images-na.ssl-images-amazon.com/images/I/41R0mDwPL-L._SX324_BO1,204,203,200_.jpg",
+      image: "https://th.bing.com/th/id/OIP.wWTlPSNLJf3MYA1VqQOOEgAAAA?pid=ImgDet&rs=1",
+    },
+    {
+      title: "Purple Hibiscus",
+      author: "Chimamanda Adichie",
+      price: 10.79,
+      image: "https://www.bibdsl.co.uk/imagegallery/bookdata/cd410/9780007189885.JPG",
+    },
+    {
+      title: "Things Fall Apart",
+      author: "Chinua Achebe",
+      price: 6.99,
+      image: "https://th.bing.com/th/id/OIP.a2bslemABy5pKGyIIkk7GAHaLX?pid=ImgDet&rs=1",
+    },
+    {
+      title: "Half of a Yellow Sun",
+      author: "Chimananda Adichie",
+      price: 9.89,
+      image: "https://images.booksense.com/images/209/095/9781400095209.jpg",
     },
   ];
   
